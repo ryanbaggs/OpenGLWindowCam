410c9850393e766c7168821817fce56a23fe48fc
Ryan Baggs <rosammons@gmail.com>
2020-11-09 23:37:32 +0100

    Add createVAO method.
    Add createVAO which creates a Vertex Array Object to hold information on
    the VBOs and vertex arrays of data.

--- OpenGL Window Cam/src/window/Renderer.java
+++ OpenGL Window Cam/src/window/Renderer.java
@@ -12,6 +12,8 @@ class Renderer {
 	
 	// Vertex Buffer Object.
 	int vbo;
+	// Vertex Array Object.
+	int vao;
 	
 	// The vertices of the triangle.
 	private float[] points = {
@@ -26,7 +28,9 @@ class Renderer {
 	
 	/**
 	 * Creates a new Vertex Buffer Object (VBO), binds it, and adds data to 
-	 * it. The vertex buffer object is what OpenGL uses to hold vertex data, 
+	 * it. 
+	 * 
+	 * The vertex buffer object is what OpenGL uses to hold vertex data, 
 	 * for example, the location or color.
 	 */
 	void createVBO() {
@@ -40,6 +44,36 @@ class Renderer {
 		// Give the array the data and inform that the data will be set only 
 		// once, but will be drawn many times.
 		GL43.glBufferData(GL43.GL_ARRAY_BUFFER, points, GL43.GL_STATIC_DRAW);
+	}
+	
+	/**
+	 * Creates a new Vertex Array Object (VAO), binds it, enables the first 
+	 * element (attribute), binds the VBO, and define the array of vertices 
+	 * (how they will be read as).
+	 * 
+	 * The VAO keeps track of the VBOs, the array definitions for each, and 
+	 * the attribute (like coordinates, or color) arrays so you don't have to 
+	 * re-bind and redefine the VAOs and arrays every time you draw the mesh.
+	 */
+	void createVAO() {
+		// Generate the VAO name.
+		vao = GL43.glGenVertexArrays();
+		
+		// Bind the VAO, making it the "current" VAO being acted upon. Only 
+		// one per OpenGL context.
+		GL43.glBindVertexArray(vao);
+		
+		// Enable the first attribute array in the VAO for use.
+		GL43.glEnableVertexAttribArray(0);
+		
+		// Bind the VBO making it the "current" VBO.
+		GL43.glBindBuffer(GL43.GL_ARRAY_BUFFER, vbo);
+		
+		// The first parameter is the index for the VAO to set the currently 
+		// bound array buffer to said index. The rest of the parameters are 
+		// info on the array buffer's data. 
+		// Parameters: index, size, type, normalized, stride, pointer (or offset).
+		GL43.glVertexAttribPointer(0, 3, GL43.GL_FLOAT, false, 0, 0L);
 	}
 	
 	void update() {
